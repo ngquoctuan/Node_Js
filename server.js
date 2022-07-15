52d8914d64242bf5c7524882f7c34d0abd29a61c
ngquoctuan <ngquoctuan2706@gmail.com>
2022-07-15 18:56:02 +0700

    Sugar Venom

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('run request ...')
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h3>Hello world! </h3>');
+    res.write('<h2>from ERIC & HOI DAN IT </h2>');
+    res.end();
+    //hehe
+})
+
+server.listen(3306, 'localhost', () => {
+    console.log('Node.JS server is running on port: 3000');
+})
