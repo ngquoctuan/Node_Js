199a661b7935fda13e6db2ab83b63b7aee5bb62a
ngquoctuan <ngquoctuan2706@gmail.com>
2022-07-15 22:17:10 +0700

    video 5

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const http = require('http');
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
-    //hehe
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hello Sugar Venom')
 })
 
-server.listen(3306, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/about', (req, res) => {
+    res.send(`I'm Sugar Venom`)
 })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
